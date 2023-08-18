ed74e5633e2933878849f54c5b15cb2941cc975b
haijaa <antkarl11@gmail.com>
2023-08-18 11:41:21 +0200

    Practiced making a loopfunction.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-console.log("Loaded javascript can't")
+console.log("Loaded javascript can't");
 function submitform(event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  let field1Value = document.getElementById("title").value;
+  let field2Value = document.getElementById("Raiting").value;
 
-    let field1Value = document.getElementById('title').value;
-    let field2Value = document.getElementById('Raiting').value;
-    
-    console.log('Field 1', field1Value);
-    console.log('Field 2', field2Value);
-
+  console.log("Field 1", field1Value);
+  console.log("Field 2", field2Value);
 }
 
 function greeting(firstname, lastname, mat) {
-    console.log("Hej" , firstname, lastname, mat)
+  console.log("Hej", firstname, lastname, mat);
 }
 
-greeting("Anton" , "Karlsson" , "Pizza")
+greeting("Anton", "Karlsson", "Pizza");
 
-
-// document.onload = (() => {
-
-//     let form = document.getElementById("form")
-//     console.log(form)
-//     form.addEventListener("submit" , submitform)
-// })
+function numbers() {
+  for (i = 1; i <= 100; i++) {
+    var currentPtag = document.createElement("p");
+    currentPtag.innerHTML = i;
+    let ListTag = document.getElementById("List");
+    ListTag.appendChild(currentPtag);
+    console.log(currentPtag);
+  }
+}
+numbers();
